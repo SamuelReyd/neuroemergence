b344e83b807ef8288734c5229ff4118968a50bd7
samuelreyd <samuel.reyd@telecom-paris.fr>
2020-11-18 16:19:44 +0100

    commit

--- Rendering.py
+++ Rendering.py
@@ -3,7 +3,7 @@ from pygame.locals import *
 
 def render(window : pygame.Surface, ball: Ball, Plateform: plateform):
     window.fill((255,255,255))
-    window.draw.rect(window, plateform.color, pygame.Rect(plateform.x, WINDOW_HEIGHT-plateform.width, plateform.length, plateform.width)
+    window.draw.rect(window, plateform.color, pygame.Rect(plateform.x, WINDOW_HEIGHT-plateform.width, plateform.length, plateform.width))
     window.draw.circle(window, ball.color, (int(ball.pos[0]), int(ball.pos[1])))
     
     pygame.display.update()
