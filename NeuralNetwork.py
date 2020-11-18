e6232692c30db88f1562a5c07222cd4f33282e79
samuelreyd <samuel.reyd@telecom-paris.fr>
2020-11-18 16:41:58 +0100

    NeuralNetwork done

---
+++ NeuralNetwork.py
@@ -0,0 +1,20 @@
+import numpy as np
+from scipy.special import expit
+
+class NeuralNetwork:
+    
+    @staticmethod
+    def get_random_neural_network(vec_sizes: list):
+        layers = list()
+        for i in range(len(vec_sizes)-1):
+            layers.append(np.random.uniform(-1,1, (vec_sizes[i+1], vec_sizes[i])))
+        return NeuralNetwork(layers)
+    
+    def __init__(self, layers: list):
+        self.layers = layers
+        
+    def feed_forward(self, input: list):
+        vec = np.array(input).T
+        for layer in self.layers:
+            vec = expit(np.dot(layer, vec))
+        return vec
