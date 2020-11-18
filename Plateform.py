4be66d734ff6768ffaf2709736a68abdb15820b3
samuelreyd <samuel.reyd@telecom-paris.fr>
2020-11-18 14:53:40 +0100

    add conctants.py

---
+++ Plateform.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+class Plateform:
+    
+    length = 30
+    
+    def __init__(self):
+        self.speed = 0
+        self.x = 0
+        
+    def update(self):
+        self.pos += self.speed
+    
+    def action_performed(dir: int):
+        speed += dir
+        
+    def 
