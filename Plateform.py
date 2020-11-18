7075c8024d24034c10117873610bd2a4999a1a90
samuelreyd <samuel.reyd@telecom-paris.fr>
2020-11-18 15:01:31 +0100

    constants added

--- Plateform.py
+++ Plateform.py
@@ -3,6 +3,7 @@ import numpy as np
 class Plateform:
     
     length = 30
+    limit_speed = 5
     
     def __init__(self):
         self.speed = 0
@@ -10,8 +11,12 @@ class Plateform:
         
     def update(self):
         self.pos += self.speed
+        self.pos = min(self.pos, WINDOW_WIDTH)
+        self.pos = max(self.pos, 0)
     
     def action_performed(dir: int):
         speed += dir
+        self.speed = min(self.speed, self.limit_speed)
+        self.speed = max(self.speed, -self.limit_speed)
         
     def 
